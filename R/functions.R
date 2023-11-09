60d70aeeda4cee268df4ddf57c130d642f07391a
Núria Estanyol-Torres <estanyol.nuria@gmail.com>
2023-11-09 11:28:20 +0100

    Added quarto file to targets

--- R/functions.R
+++ R/functions.R
@@ -10,3 +10,18 @@ descriptive_stats <- function(data) {
         dplyr::summarise(dplyr::across(value, list(mean = mean, sd = sd))) %>%
         dplyr::mutate(dplyr::across(where(is.numeric), ~ round(.x, digits = 1)))
 }
+
+#' Plot distributions
+#'
+#' @param data
+#'
+#' @return A plot object.
+
+plot_distributions <- function(data) {
+    data %>%
+        ggplot2::ggplot(ggplot2::aes(x = value)) +
+        ggplot2::geom_histogram() +
+        ggplot2::facet_wrap(ggplot2::vars(metabolite), scales = "free")
+}
+
+
